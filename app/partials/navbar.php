86323147a80fffec258a289a3325dae65e18bc39
Oluwatimilehin Orawusi <230201433@aston.ac.uk>
2024-11-12 15:52:53 +0000

    Added the Navbar partial

---
+++ app/partials/navbar.php
@@ -0,0 +1,12 @@
+<nav>
+            <div class="nav-left">
+                <p id="logo-text">GADGET GARDEN</p>
+            </div>
+            <div class="nav-right">
+                <button id="categories-button">Categories </button>
+                <button class="white-button">About Us</button>
+                <button class="green-button">Login</button>
+                <button class="white-button">Sign Up</button>
+                
+            </div>
+</nav>

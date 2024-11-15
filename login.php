bf863cfccc6e4c2a8b98a901215390a7b1dfa9bb
Luca Corcione <230210859@aston.ac.uk>
2024-11-15 22:50:37 +0000

    File created
    File for log in page created and initial programming has begun

---
+++ login.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign in</title>
+</head>
+<body>
+    <?php require navbar.php ?>
+    <h1 id = "catchphrase"> Grow Your Tech Sustainably - Buy, Sell, and Renew at Gadget Garden!</h1>
+    
+</body>
+</html>

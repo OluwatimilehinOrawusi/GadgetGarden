f92509f4b4bfb79ad1dba793ac7a7ba25b59d9d9
Oluwatimilehin Orawusi <230201433@aston.ac.uk>
2024-11-18 20:46:17 +0000

    Added css file for navbar

---
+++ public/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include 'header.php' ?>
+    </head>
+    <body>
+    <section id="hero-section">
+        <nav>
+            <div class="nav-left">
+                <p id="logo-text">GADGET GARDEN</p>
+            </div>
+            <div class="nav-right">
+                <button id="categories-button">CATEGORIES</button>
+                <button class="white-button">ABOUT US</button>
+                <button class="green-button">LOGIN</button>
+                <button class="white-button">SIGN UP</button>
+                
+            </div>
+        </nav>
+        <div id="landing-display-container">
+            <div id="landing-display">
+                <div id="hero-main-img" class="home-images">
+                    <h1 id="title-text">GADGET GARDEN</H1>
+                </div>
+                <div id="home-top-right" class="home-images">
+                    <img>
+                </div>
+                <div id="home-bottom-right" class="home-images">
+                    <img>
+                </div>        
+            </div>
+        </div>
+        <hr>
+    </section>
+    <section style="height:100rem"></section>
+    
+    </body>
+</html>

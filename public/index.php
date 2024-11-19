67f62822cdf89f1f973a6bb7fec0560f6863679b
Oluwatimilehin Orawusi <230201433@aston.ac.uk>
2024-11-19 15:26:02 +0000

    added partials

--- public/index.php
+++ public/index.php
@@ -6,18 +6,7 @@
     <body>
         <!-- Hero Section -->
     <section id="hero-section">
-        <nav>
-            <div class="nav-left">
-                <p id="logo-text">GADGET GARDEN</p>
-            </div>
-            <div class="nav-right">
-                <button id="categories-button">CATEGORIES</button>
-                <button class="white-button">ABOUT US</button>
-                <button class="green-button">LOGIN</button>
-                <button class="white-button">SIGN UP</button>
-                
-            </div>
-        </nav>
+        <?php require_once "../app/partials/navbar.php"?>
         <div id="landing-display-container">
             <div id="landing-display">
                 <div id="hero-main-img" class="home-images">
@@ -105,47 +94,7 @@
     </section>
 
     
-    <!-- Footer -->
-    <footer class="footer">
-        <div class="footer-content">
-            <h3>GADGET GARDEN</h3>
-            <p>Your go-to store for everything tech.</p>
-            <form class="footer-form">
-                <input type="text" placeholder="Type your questions">
-                <button style="border: 1px solid white;" class="green-button" type="submit">Submit</button>
-            </form>
-        </div>
-        <div class="footer-links">
-            <div class="footer-column">
-                <h4>Popular</h4>
-                <a class="footer-link" href="#">Phones</a>
-                <a class="footer-link" href="#">Tablets</a>
-                <a  class="footer-link"href="#">Laptops</a>
-                <a  class="footer-link"href="#">Audio</a>
-                <a class="footer-link" href="#">Accessories</a>
-            </div>
-            <div class="footer-column">
-                <h4>Menu</h4>
-                <a class="footer-link" href="#">All Categories</a>
-                <a class="footer-link"href="#">Gift Cards</a>
-                <a class="footer-link"href="#">Special Events</a>
-                <a  class="footer-link"href="#">Testimonials</a>
-                <a class="footer-link"href="#">Blog</a>
-            </div>
-            <div class="footer-column">
-                <h4>Other</h4>
-                <a class="footer-link" href="#">Tracking Package</a>
-                <a class="footer-link"href="#">FAQ</a>
-                <a class="footer-link"href="#">About Us</a>
-                <a  class="footer-link"href="#">Contact Us</a>
-                <a class="footer-link" href="#">Terms and Conditions</a>
-            </div>
-        </div>
-    </footer>
-
-</html>
-
+    <?php require_once "../app/partials/footer.php" ?>
     
     </body>
-</html>
 </html>

80dc9d60de5ca357f0f1146fb93b8d8597f1605e
AHMED011677 <230316874@aston.ac.uk>
2024-11-24 00:04:22 +0000

    Update db_connection.php

--- database/db_connection.php
+++ database/db_connection.php
@@ -1,18 +1,16 @@
 <?php
 
-    //conncection parameters for the database
-    $db_host = 'localhost';
-    $db_name = 'cs2team33_db'; //The name of the database that we will be using for the gadget garden
-    $username = 'cs2team33'; //The username for the database. This was given to the team.
-    $password = 'ILINC7qEIJdMtba'; //The password to access the database
+$dbhost = 'localhost';
+$dbname = 'cs2team33_db';
+$username = 'cs2team33';
+$password = 'ILINC7qEIJdMtba';
 
-
-    try {
-        $db = new PDO("mysql:dbname=$db_name;host=$db_host", $username, $password);
-    } catch (PDOException $ex) {
-        // If connection fails, display error message and exit script
-        echo("Failed to connect to the database.<br>");
-        echo($ex->getMessage());
-        exit;
-    }
-?>
+try {
+	$db = new PDO("mysql:host=$dbhost;dbname=$dbname", $username,$password);
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $ex) {
+	echo("Failed to connect to the database.<br>");
+	echo($ex->getMessage());
+	exit;
+}
+?> 

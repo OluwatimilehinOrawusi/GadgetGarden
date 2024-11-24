21780e682cd569319e18db51a2f86bcc408667d2
Oluwatimilehin Orawusi <230201433@aston.ac.uk>
2024-11-24 18:32:26 +0000

    Made changes to navbar

--- partials/navbar.php
+++ partials/navbar.php
@@ -1,13 +1,23 @@
+<?php
+session_start();
 
+?>
 <nav>
             <div class="nav-left">
-                <p id="logo-text">GADGET GARDEN</p>
+                <a href="../index.php"><p id="logo-text">GADGET GARDEN</p></a>
             </div>
             <div class="nav-right">
-                <button id="categories-button">Categories </button>
-                <button class="white-button">About Us</button>
-                <button class="green-button">Login</button>
-                <button class="white-button">Sign Up</button>
-                
+                <a href="../views/products.php"><button class="green-button" >Products</button></a>
+                <a href="#categories"><button class="white-button">About Us</button></a>
+                <?php if (!isset($_SESSION['user_id'])){?>
+                <?php echo '<a href="./login.php"><button class="green-button">Login</button></a>' ?>
+                 <?php echo '<a href="./signup.php"><button class="white-button">Sign Up</button></a> '?>
+                <?php }?>
+                <?php if (isset($_SESSION['user_id'])){?>
+                <?php echo '<a href="./basket.php"><button class="green-button">Basket</button></a>' ?>
+                <?php echo '<a href="./logout.php"><button class="white-button">Logout</button></a>' ?>
+
+                <?php }?>
+
             </div>
 </nav>

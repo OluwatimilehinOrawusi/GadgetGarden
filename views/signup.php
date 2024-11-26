1a003ab60979914b7604beb994b68434f053e5bd
Luca Corcione <230210859@aston.ac.uk>
2024-11-26 03:06:07 +0000

    Modified some of the html in signup.php just to make things easier to use and tidied up a little bit

--- views/signup.php
+++ views/signup.php
@@ -1,3 +1,6 @@
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -47,28 +50,28 @@
         <div class = "signup">
             <h3>Create Account</h3>
 
-            <form id ="myForm" onsubmit="return checkEmails(this)">
+            <form method='POST' action='./signup.php' id ="myForm" >
 
                 <div class = "creating">    
                     <label>Username</label>
-                    <input required type="text" id="name" placeholder="Username">
+                    <input required type="text" id="name" name="username" placeholder="Username">
 
                     <label for="email">E-mail</label>
 
-  <input required type="text" id="email1" name="Email" placeholder="Email">
+  <input required type="text" id="email1" name="email" placeholder="email">
 
   <label for="phonenumber">Phone Number</label>
 
-  <input required ="number" id="phonenumber" name="Phone Number" placeholder="Phone Number">
+  <input required ="number" id="phonenumber" name="phone" placeholder="phone">
 
 
   <label for="password">Password</label>
 
-  <input required type = "password" id="password" name="Password" placeholder="Password">
+  <input required type = "password" id="password" name="password" placeholder="password">
 
   <label for="cpassword">Confirm Password</label>
 
-  <input required ="text" id="cpassword" name="Confirm Password" placeholder="Confirm Password">
+  <input required ="text" id="cpassword" name="confirm_password" placeholder="confirm_password">
 
   <button type="createaccount">Create Account</button>
 

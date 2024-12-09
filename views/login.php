2f80425bedc929f51a21283cf2db5cb36cd1fbe9
AHMED011677 <230316874@aston.ac.uk>
2024-12-09 13:08:55 +0000

    Fixed the nav bar so contct us only appears upon login

--- views/login.php
+++ views/login.php
@@ -35,7 +35,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 <a href="../index.php"><p id="logo-text">GADGET GARDEN</p></a>
             </div>
             <div class="nav-right">
-                <a href="../views/contact.php"><button class="green-button" >Contact Us</button></a>
                 <a href="../views/aboutpage.php"><button class="white-button">About Us</button></a>
                 <?php if (!isset($_SESSION['user_id'])){?>
                 <?php echo '<a href="./login.php"><button class="green-button">Login</button></a>' ?>
@@ -43,6 +42,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 <?php }?>
                 <?php if (isset($_SESSION['user_id'])){?>
                 <?php echo '<a href="./basket.php"><button class="white-button">Basket</button></a>' ?>
+                <?php echo '<a href="./contact.php"><button class="white-button">Contact us</button></a>' ?>
                 <?php echo '<a href = "./profile.php"><button class ="white-button">Profile</button></a>' ?>
                 <?php echo '<a href="./logout.php"><button class="green-button">Logout</button></a>' ?>
 

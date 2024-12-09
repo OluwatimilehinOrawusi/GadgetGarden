21cdf459402f73dc43d04f4982cc082ca2ccd7f7
Luca Corcione <230210859@aston.ac.uk>
2024-12-09 12:20:53 +0000

    double contact us page in aboutpage.php nav bar

--- views/aboutpage.php
+++ views/aboutpage.php
@@ -34,7 +34,6 @@ $pdo = require_once "../database/database.php";
                 <?php }?>
                 <a href="../views/products.php"><button class="green-button" >Products</button></a>
                 <?php if (isset($_SESSION['user_id'])){?>
-                <?php echo '<a href="./contact.php"><button class="green-button">Contact us</button></a>' ?>
                 <?php echo '<a href="./basket.php"><button class="white-button">Basket</button></a>' ?>
                 <?php echo '<a href = "./profile.php"><button class ="white-button">Profile</button></a>' ?>
                 <?php echo '<a href="./logout.php"><button class="green-button">Logout</button></a>' ?>

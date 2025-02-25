1841aac2ac784d9b4c834154cd507a94372ea673
HammadAli230 <230216770@aston.ac.uk>
2025-02-25 04:51:10 +0000

    stock now works
    when something is bought the stock drops

--- database/db_connection.php
+++ database/db_connection.php
@@ -1,18 +1,14 @@
 <?php
+$db_host = 'localhost';
+$db_name = 'gadgetgarden';
+$username = 'root';
+$password = '';
 
-    //conncection parameters for the database
-    $db_host = 'localhost';
-    $db_name = 'gadet garden'; //The name of the database that we will be using for the gadget garden
-    $username = 'root'; //The username for the database. This was given to the team.
-    $password = ''; //The password to access the database
-
-
-    try {
-        $db = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8mb4", $username, $password);
-    } catch (PDOException $ex) {
-        // If connection fails, display error message and exit script
-        echo("Failed to connect to the database.<br>");
-        echo($ex->getMessage());
-        exit;
-    }
+try {
+    $db = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8mb4", $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $db; 
+} catch (PDOException $ex) {
+    die("Failed to connect to the database. Error: " . $ex->getMessage()); 
+}
 ?>

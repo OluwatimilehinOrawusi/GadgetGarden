502c275e3a3d47af207dd3b6af7556b7b5308e1f
AHMED011677 <230316874@aston.ac.uk>
2025-03-08 15:42:04 +0000

    nav bar added and redirection changed after product updated

--- views/edit-product.php
+++ views/edit-product.php
@@ -18,7 +18,6 @@ exit();
 
 //If product id is included in the URL
 if (!isset($_GET['product_id']) || !is_numeric($_GET['product_id'])) {
-    die("Error: Invalid product ID."); // Temporary debugging message
     header("Location: admin.php");
     exit();
 }
@@ -52,7 +51,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
         $stmt->execute([$name, $price, $description, $stock, $image, $category_id, $product_id]);
 
 
-        header("Locationm: admin.php");
+        header("Location: admin.php");
         exit();
     }
 }
@@ -66,6 +65,28 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
         <link rel="stylesheet" href="../public/css/styles.css">
     </head>
     <body>
+    <nav>
+    <!---nav bar-->
+            <div class="nav-left">
+                <a href="../index.php"><p id="logo-text">GADGET GARDEN</p></a>
+            </div>
+            <div class="nav-right">
+                <a href="./contact.php"><button class="green-button" >Contact Us</button></a>
+                <a href="./aboutpage.php"><button class="white-button">About Us</button></a>
+                <?php if (!isset($_SESSION['user_id'])){?>
+                <?php echo '<a href="./login.php"><button class="green-button">Login</button></a>' ?>
+                 <?php echo '<a href="./signup.php"><button class="white-button">Sign Up</button></a> '?>
+                <?php }?>
+                <a href="../views/products.php"><button class="green-button" >Products</button></a>
+                <?php if (isset($_SESSION['user_id'])){?>
+                <?php echo '<a href="./basket.php"><button class="white-button">Basket</button></a>' ?>
+                <?php echo '<a href = "./profile.php"><button class ="white-button">Profile</button></a>' ?>
+                <?php echo '<a href="./logout.php"><button class="green-button">Logout</button></a>' ?>
+
+                <?php }?>
+
+            </div>
+</nav>
         <div class="container">
             <h1>Edit Product</h1>
             <form method="POST">

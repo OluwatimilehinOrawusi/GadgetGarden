9520e98e154e38c76fc0f313f407fc5e0abddfa9
AHMED011677 <230316874@aston.ac.uk>
2025-03-08 16:22:20 +0000

    css completed for edit form

--- views/edit-product.php
+++ views/edit-product.php
@@ -69,26 +69,21 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     </head>
     <body>
     <nav>
-    <!---nav bar-->
-            <div class="nav-left">
-                <a href="../index.php"><p id="logo-text">GADGET GARDEN</p></a>
-            </div>
-            <div class="nav-right">
-                <a href="./contact.php"><button class="green-button" >Contact Us</button></a>
-                <a href="./aboutpage.php"><button class="white-button">About Us</button></a>
-                <?php if (!isset($_SESSION['user_id'])){?>
-                <?php echo '<a href="./login.php"><button class="green-button">Login</button></a>' ?>
-                 <?php echo '<a href="./signup.php"><button class="white-button">Sign Up</button></a> '?>
-                <?php }?>
-                <a href="../views/products.php"><button class="green-button" >Products</button></a>
-                <?php if (isset($_SESSION['user_id'])){?>
-                <?php echo '<a href="./basket.php"><button class="white-button">Basket</button></a>' ?>
-                <?php echo '<a href = "./profile.php"><button class ="white-button">Profile</button></a>' ?>
-                <?php echo '<a href="./logout.php"><button class="green-button">Logout</button></a>' ?>
-
-                <?php }?>
-
-            </div>
+    <div class="nav-left">
+        <a href="../index.php"><p id="logo-text">GADGET GARDEN</p></a>
+    </div>
+    <div class="nav-right">
+        <a href="../views/aboutpage.php"><button class="white-button">About Us</button></a>
+        <?php if (!isset($_SESSION['user_id'])) { ?>
+            <a href="./login.php"><button class="green-button">Login</button></a>
+            <a href="./signup.php"><button class="white-button">Sign Up</button></a>
+        <?php } else { ?>
+            <a href="./basket.php"><button class="white-button">Basket</button></a>
+            <a href="./contact.php"><button class="white-button">Contact Us</button></a>
+            <a href="./profile.php"><button class="white-button">Profile</button></a>
+            <a href="./logout.php"><button class="green-button">Logout</button></a>
+        <?php } ?>
+    </div>
 </nav>
         <div class="container">
             <h1>Edit Product</h1>

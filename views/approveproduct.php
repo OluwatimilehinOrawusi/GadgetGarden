2f5bcd78371674561e6498bdb02044d6a995f774
Kojo6771 <230165858@aston.ac.uk>
2025-03-11 15:32:46 +0000

    Added comments for readability

--- views/approveproduct.php
+++ views/approveproduct.php
@@ -1,18 +1,24 @@
-<?php require_once "../database/database.php";
+<?php 
+//Connect to database
+require_once "../database/database.php";
 
 if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['product_id'], $_POST['action'])) {
+    //Storing product ID into variable
     $product_id = $_POST['product_id'];
     $action = $_POST['action'];
 
+    //Block of code to run if the approve button is pressed
     if ($action === "approve") { $stmt = $pdo->prepare("SELECT * FROM upload_products WHERE product_id = ?");
         $stmt->execute([$product_id]);
         $product=$stmt->fetch(PDO::FETCH_ASSOC);
 
+        //Insert the uploaded product into the products table
         if ($product) { $stmt = $pdo->prepare("
         INSERT INTO products(product_id,name,description,price,stock,state,category_id,image)
         VALUES (?,?,?,?,?,?,?,?)
         ");
 
+        //The values from the upload products table which will be inserted into the products table
         $stmt->execute([
             $product['product_id'],
             $product['name'],
@@ -25,11 +31,14 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['product_id'], $_POST[
 
 
         ]);
-
+        //Delete the uploaded product from the upload product table after successful insert into the products table
         $stmt = $pdo->prepare("DELETE FROM upload_products WHERE product_id = ?"); $stmt->execute([$product_id]);
     }
 }
-else { $stmt = $pdo->prepare("DELETE FROM upload_products WHERE product_id = ?"); $stmt->execute([$product_id]); }
+else { 
+     //Delete the uploaded product from the upload product table
+    $stmt = $pdo->prepare("DELETE FROM upload_products WHERE product_id = ?"); $stmt->execute([$product_id]); 
+}
 
 }
 header("Location: ReviewCustomerUploads.php ");

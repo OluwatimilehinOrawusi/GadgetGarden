8986c3a0ea2d876289883dec1c8520aebc9bfb1b
Luca Corcione <230210859@aston.ac.uk>
2025-03-18 13:02:33 +0000

    html code for wishlist.php and php code has started.

--- views/wishlist.php
+++ views/wishlist.php
@@ -6,6 +6,24 @@
     <title>Wishlist</title>
 </head>
 <body>
+    <div class = "wishlistcon">
+        <h1>Bookmarked Items</h1>
+        <?php if ($items) : ?>
+            <?php foreach ($items as $item): ?>
+                <div clas = "wishlistcrd">
+                    <img src="<?php echo htmlspecialchars($item['image']); ?>" alt = "Image">
+                    <h3><?php echo htmlspecialchars($item['name']); ?></h3>
+                    <p>£<?php echo htmlspecialchars($item['price']); ?></p>
+                    <a href = "productPage.php?id=<?php echo $item['product_id']; ?>"><button class = "green-button">View</button></a>
+                     <!-- add remove wishlist link/page  -->
+            </div>
+            <?php endforeach; ?>
+                </div>
+                <?php else: ?>
+                    <h5>You have no bookmarked items</h5>
+                    <?php endif; ?>
+    </div>
     
+    <?php require_once "../partials/footer.php"
 </body>
 </html>

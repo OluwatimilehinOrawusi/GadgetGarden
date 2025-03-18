c502ed785ddced65acf56bf0bb290a24800149c8
Luca Corcione <230210859@aston.ac.uk>
2025-03-18 01:04:01 +0000

    created the code for bookmark.php so once people click on the add to wishlist button it adds it to the database to be seen later on their wishlist page once it is created

---
+++ views/bookmark.php
@@ -0,0 +1,35 @@
+<?php
+
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+$pdo = require_once "../database/database.php";
+
+try {
+    if (!isset($_SESSION['user_id'])) {
+        throw new Exception("User ID is not set in session.");
+    }
+
+    if (!isset($_GET['product_id']) || !is_numeric($_GET['product_id'])) {
+        throw new Exception("Invalid product ID");
+    }
+
+    $user_id = $_SESSION['user_id'];
+    $product_id = intval($_GET['product_id']);
+
+    $statement = $pdo->prepare(' INSERT IGNORE INTO wishlist (user_id, product_id) VALUES (:user_id, :product_id)');
+
+    $statement->bindValue(":user_id", $user_id, PDO::PARAM_INT);
+    $statement->bindValue(":product_id", $product_id, PDO::PARAM_INT);
+    $statement->execute();
+    if($statement->rowCount()>0) {
+        echo "Product has been bookmarked successfully<br>";
+        echo "<a href = './wishlist.php'>Open Wishlist</a> ";
+         echo "<a href='./products.php'>Continue Shopping</a>";
+    } else {
+        throw new Exception("Failed to add the product to the wishlist.");
+    }
+
+}
--- views/product.php
+++ views/product.php
@@ -64,7 +64,9 @@ function displayStars($rating) {
             <p class="product-condition">Condition: <?php echo htmlspecialchars($product["state"]) ?></p>
             <p class="product-price">£<?php echo htmlspecialchars($product["price"]) ?></p>
             <a href="add-products.php?product_id=<?php echo $product["product_id"] ?>"><button class="green-button">Add to Basket</button></a>
+            
             <a href="./reviewPage.php?id=<?php echo $id ?>"><u>Write a review</u></a>
+
         </div>
     </div>
 

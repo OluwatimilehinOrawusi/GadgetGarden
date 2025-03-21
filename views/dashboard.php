167fb26f13470ee7c6e6a7d63f3d15828a2b5227
Kojo6771 <230165858@aston.ac.uk>
2025-03-21 03:13:56 +0000

    Added comments and spacing for readability
    I commented all the sections of the dashboard page so it easier to read for collaborative purposes

--- views/dashboard.php
+++ views/dashboard.php
@@ -3,6 +3,7 @@ session_start();
 
 $pdo = require_once "../database/database.php";
 
+//Stores the user ID from the session
 $user_id = $_SESSION['user_id'] ?? null;
 
 // Fetch the admin status of the user
@@ -12,6 +13,8 @@ $stmt->execute();
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
 ?>
 
+
+<!-----HTML----->
 <!DOCTYPE html>
 <html>
     <head>
@@ -23,6 +26,7 @@ $user = $stmt->fetch(PDO::FETCH_ASSOC);
     </head>
     <body>
      
+    <!-- Nav Bar -->
     <nav>
     <div class="nav-left">
         <a href="../index.php"><p id="logo-text">GADGET GARDEN</p></a>
@@ -35,56 +39,69 @@ $user = $stmt->fetch(PDO::FETCH_ASSOC);
         <a href="./logout.php"><button class="green-button">Logout</button></a>
     </div>
 </nav>
+
+
     <div id="main-container">
     <h1> Welcome <?php echo $user['username'] ?> !</h1>
     <p>Administration Dashboard</p>
         <div id="grid-container">
-                    
+        
+        <!-- Orders card -->
         <a href="./manage_orders.php" class="dashboard-cards clickable-div">
             <img src="../public/assets/Quickdelivery.png">
             <p>Orders</p>
         </a>
 
+        <!-- products review card -->
         <a href="./adminReviews.php" class="dashboard-cards clickable-div">
             <img src="../public/assets/Checkbox.png">
             <p>Product Reviews</p>
         </a>
 
+        <!-- Review customer uploads card -->
         <a href="./ReviewCustomerUploads.php" class="dashboard-cards clickable-div">
             <img src="../public/assets/Download.png">
             <p>Review Customer Uploads</p>
         </a>
 
+        <!-- Legal card -->
         <a href="./legal.php" class="dashboard-cards clickable-div">
             <img src="../public/assets/Invoice.png">
             <p>Legal</p>
         </a>
 
+        <!-- Inventory Management card -->
         <a href="./admin.php" id="dashboard-links" class="dashboard-cards clickable-div">
             <img src="../public/assets/Home.png">
             <p>Inventory Management</p>
         </a>
 
+        <!-- Analytics card -->
         <a href="./admin_dashboard.php" id="analytic-card" class="dashboard-cards clickable-div">
             <img src="../public/assets\Diagram.png">
             <p>Analytics</p>
         </a>
 
+
+        <!-- Alerts card -->
         <a href="./alerts.php" class="dashboard-cards clickable-div">
             <img src="../public/assets/VolumeUp.png">
             <p>Alerts</p>
         </a>
 
+        <!-- Customer Queries card -->
         <a href="./admin_contact.php" class="dashboard-cards clickable-div">
             <img src="../public/assets/Account.png">
             <p>Customer Queries</p>
         </a>
 
+        <!-- customer details card -->
         <a href="./manage_users.php" class="dashboard-cards clickable-div">
             <img src="../public/assets/Partner.png">
             <p>Customer Details</p>
         </a>
 
+        <!-- Customer forum card -->
         <a href="./replies.php" class="dashboard-cards clickable-div">
             <img src="../public/assets/Megaphone.png">
             <p>Customer Forum</p>

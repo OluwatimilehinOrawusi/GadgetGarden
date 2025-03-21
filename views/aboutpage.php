df9001aafc339410ec09ed6a9f30d0c3d7114240
Carlos Tony <230152146@aston.ac.uk>
2025-03-21 13:48:03 +0000

    Update aboutpage.php

--- views/aboutpage.php
+++ views/aboutpage.php
@@ -17,9 +17,6 @@ $pdo = require_once "../database/database.php";
     <link rel="stylesheet" href="../public/css/navbar.css">
     <link rel="stylesheet" href="../public/css/styles.css">
     <link rel="stylesheet" href="../public/css/chatbot.css">
-    <link rel="stylesheet" href="../public/css/darkmode.css">
-
-     <script src="../public/js/darkmode.js"></script>
 
 
 </head>
@@ -38,7 +35,6 @@ $pdo = require_once "../database/database.php";
                 <a href="./contact.php"><button class="white-button">Contact Us</button></a>
                 <a href="./profile.php"><button class="white-button">Profile</button></a>
                 <a href="./logout.php"><button class="green-button">Logout</button></a>
-                <button id="darkModeToggle">Toggle Dark Mode</button>
             <?php } ?>
         </div>
     </nav>

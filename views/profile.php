8f6aaa9fde0e3534cd5a3ca64f809644533fa3a1
Carlos Tony <230152146@aston.ac.uk>
2025-03-21 20:04:50 +0000

    Update profile.php

--- views/profile.php
+++ views/profile.php
@@ -51,6 +51,7 @@ $orders = $orderQuery->fetchAll(PDO::FETCH_ASSOC);
     <?php require '../partials/header.php'; ?>
     <link rel="stylesheet" href="../public/css/profile.css">
     <link rel="stylesheet" href="../public/css/chatbot.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
 <body>
 <nav>

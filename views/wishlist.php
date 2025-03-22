5d8ce2c16cbfcb7f13662732d8a9849e9e23109f
Luca Corcione <230210859@aston.ac.uk>
2025-03-22 02:50:24 +0000

    updated navbar for wishlist.php

--- views/wishlist.php
+++ views/wishlist.php
@@ -26,25 +26,32 @@ $items = $stmt->fetchAll(PDO::FETCH_ASSOC);
 </head>
 <body>
 <nav>
-            <div class="nav-left">
-                <a href="../index.php"><p id="logo-text">GADGET GARDEN</p></a>
-            </div>
-            <div class="nav-right">
-                <a href="../views/aboutpage.php"><button class="white-button">About Us</button></a>
-                <?php if (!isset($_SESSION['user_id'])){?>
-                <?php echo '<a href="./login.php"><button class="green-button">Login</button></a>' ?>
-                 <?php echo '<a href="./signup.php"><button class="white-button">Sign Up</button></a> '?>
-                <?php }?>
-                <?php if (isset($_SESSION['user_id'])){?>
-                <?php echo '<a href="./basket.php"><button class="white-button">Basket</button></a>' ?>
-                <?php echo '<a href="./contact.php"><button class="white-button">Contact us</button></a>' ?>
-                <?php echo '<a href = "./profile.php"><button class ="white-button">Profile</button></a>' ?>
-                <?php echo '<a href="./logout.php"><button class="green-button">Logout</button></a>' ?>
+    <div class="nav-left">
+        <a href="../index.php"><p id="logo-text">GADGET GARDEN</p>
+    </div>
+    <div class="nav-right">
+        <a href="./aboutpage.php"><button class="white-button">About Us</button></a>
 
-                <?php }?>
+        <?php if (!isset($_SESSION['user_id'])) { ?>
+            <a href="./login.php"><button class="green-button">Login</button></a>
+            <a href="./signup.php"><button class="white-button">Sign Up</button></a>
+        <?php } ?>
 
-            </div>
-</nav>       
+        <?php if (isset($_SESSION['user_id'])) { ?>
+            <a href="./basket.php"><button class="green-button">Basket</button></a>
+            <a href="./contact.php"><button class="green-button">Contact Us</button></a>
+            <a href="./profile.php"><button class="white-button">Profile</button></a>
+            <a href="./products.php"><button class="green-button">Products</button></a>
+
+            <?php if ($user && ($user['role'] === 'admin' || $user['role'] === 'manager')){ ?>
+                <a href="./dashboard.php"><button class="white-button">Admin Dashboard</button></a>
+            <?php } ?>
+
+            <a href="./logout.php"><button class="green-button">Logout</button></a>
+        
+        <?php } ?>
+    </div>
+</nav>     
 <h1>Bookmarked Items</h1>
     <div class = "wishlistcon">
         

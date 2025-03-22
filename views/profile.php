15dd98403b4126c54682891623d963f86911f35d
Kojo6771 <230165858@aston.ac.uk>
2025-03-22 02:31:47 +0000

    fixed logo linking on profile page

--- views/profile.php
+++ views/profile.php
@@ -55,7 +55,7 @@ $orders = $orderQuery->fetchAll(PDO::FETCH_ASSOC);
 <body>
 <nav>
     <div class="nav-left">
-        <p id="logo-text">GADGET GARDEN</p>
+        <a href="../index.php"><p id="logo-text">GADGET GARDEN</p>
     </div>
     <div class="nav-right">
         <a href="./aboutpage.php"><button class="white-button">About Us</button></a>

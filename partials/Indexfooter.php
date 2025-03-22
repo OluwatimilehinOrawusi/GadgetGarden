1d8f17c10bcf00f4535ae7e8418d5a8ac40b0632
Kojo6771 <230165858@aston.ac.uk>
2025-03-22 20:40:40 +0000

    Simplified index footer to match footer
    I have simplified both the index footer and the footer so that they only display pages that exist

--- partials/Indexfooter.php
+++ partials/Indexfooter.php
@@ -6,21 +6,14 @@
         </div>
         <div class="footer-links">
             <div class="footer-column">
-                <h4>Popular</h4>
+                <h4>Menu</h4>
+                <a class="footer-link" href="./views/products.php">All Categories</a>
                 <a class="footer-link" href="./views/products.php?category=phones">Phones</a>
                 <a class="footer-link" href="./views/products.php?category=tablets">Tablets</a>
                 <a  class="footer-link"href="./views/products.php?category=laptops">Laptops</a>
-                <a  class="footer-link"href="./views/products.php?category=audio">Audio</a>
-                <a class="footer-link" href="./views/products.php?category=accessories">Accessories</a>
+   
             </div>
-            <div class="footer-column">
-                <h4>Menu</h4>
-                <a class="footer-link" href="./views/products.php">All Categories</a>
-                <a class="footer-link"href="#">Gift Cards</a>
-                <a class="footer-link"href="#">Special Events</a>
-                <a  class="footer-link"href="#">Testimonials</a>
-                <a class="footer-link"href="#">Blog</a>
-            </div>
+   
             <div class="footer-column">
                 <h4>Other</h4>
                 <a class="footer-link"href="./views/replies.php">FAQ</a>

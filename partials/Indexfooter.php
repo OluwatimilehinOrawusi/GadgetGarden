72a11cc17251add86c8a008046df5631127eb045
Kojo6771 <230165858@aston.ac.uk>
2025-03-22 20:19:04 +0000

    created separate for index page
    I created a separate footer for the index page to prevent the linking clashes that arise

--- partials/Indexfooter.php
+++ partials/Indexfooter.php
@@ -7,15 +7,15 @@
         <div class="footer-links">
             <div class="footer-column">
                 <h4>Popular</h4>
-                <a class="footer-link" href="./products.php?category=phones">Phones</a>
-                <a class="footer-link" href="./products.php?category=tablets">Tablets</a>
-                <a  class="footer-link"href="./products.php?category=laptops">Laptops</a>
-                <a  class="footer-link"href="./products.php?category=audio">Audio</a>
-                <a class="footer-link" href="./products.php?category=accessories">Accessories</a>
+                <a class="footer-link" href="./views/products.php?category=phones">Phones</a>
+                <a class="footer-link" href="./views/products.php?category=tablets">Tablets</a>
+                <a  class="footer-link"href="./views/products.php?category=laptops">Laptops</a>
+                <a  class="footer-link"href="./views/products.php?category=audio">Audio</a>
+                <a class="footer-link" href="./views/products.php?category=accessories">Accessories</a>
             </div>
             <div class="footer-column">
                 <h4>Menu</h4>
-                <a class="footer-link" href="./products.php">All Categories</a>
+                <a class="footer-link" href="./views/products.php">All Categories</a>
                 <a class="footer-link"href="#">Gift Cards</a>
                 <a class="footer-link"href="#">Special Events</a>
                 <a  class="footer-link"href="#">Testimonials</a>

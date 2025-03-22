972c6a7d94513ea363bff5ff8468fda8773a7bf6
Kojo6771 <230165858@aston.ac.uk>
2025-03-22 23:59:36 +0000

    fixed spacing

--- partials/footer.php
+++ partials/footer.php
@@ -10,8 +10,8 @@
                 <a class="footer-link" href="./products.php">All Categories</a>
                 <a class="footer-link" href="./products.php?category=phones">Phones</a>
                 <a class="footer-link" href="./products.php?category=tablets">Tablets</a>
-                <a  class="footer-link"href="./products.php?category=laptops">Laptops</a>
-                <a  class="footer-link"href="./products.php?category=gaming">Gaming</a>
+                <a class="footer-link"href="./products.php?category=laptops">Laptops</a>
+                <a class="footer-link"href="./products.php?category=gaming">Gaming</a>
 
             </div>
 
@@ -19,7 +19,7 @@
                 <h4>Other</h4>
                 <a class="footer-link"href="./replies.php">FAQ</a>
                 <a class="footer-link"href="./aboutpage.php">About Us</a>
-                <a  class="footer-link"href="./contact.php">Contact Us</a>
+                <a class="footer-link"href="./contact.php">Contact Us</a>
                 <a class="footer-link" href="./legal.php">Terms and Conditions</a>
             </div>
         </div>

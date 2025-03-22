0778adc4f4dad8bb3dd19237b65723e7e330ba20
Luca Corcione <230210859@aston.ac.uk>
2025-03-22 02:39:11 +0000

    added link to index.php on login.php

--- views/login.php
+++ views/login.php
@@ -54,7 +54,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <nav>
     <div class="nav-left">
-        <p id="logo-text">GADGET GARDEN</p>
+        <a href="../index.php"><p id="logo-text">GADGET GARDEN</p>
     </div>
     <div class="nav-right">
         <a href="./aboutpage.php"><button class="white-button">About Us</button></a>

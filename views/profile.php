6937c63e77e6580428f9fd000cf7f0afd3a45db5
Luca Corcione <230210859@aston.ac.uk>
2025-03-22 01:06:56 +0000

    fixed navbar on profile.php

--- views/profile.php
+++ views/profile.php
@@ -58,18 +58,22 @@ $orders = $orderQuery->fetchAll(PDO::FETCH_ASSOC);
         <a href="../index.php"><p id="logo-text">GADGET GARDEN</p></a>
     </div>
     <div class="nav-right">
+        <a href="../views/contact.php"><button class="green-button">Contact Us</button></a>
         <a href="../views/aboutpage.php"><button class="white-button">About Us</button></a>
         <?php if (!isset($_SESSION['user_id'])) { ?>
             <a href="./login.php"><button class="green-button">Login</button></a>
             <a href="./signup.php"><button class="white-button">Sign Up</button></a>
-        <?php } else { ?>
+        <?php } ?>
+        <a href="../views/products.php"><button class="green-button">Products</button></a>
+        <?php if (isset($_SESSION['user_id'])) { ?>
             <a href="./basket.php"><button class="white-button">Basket</button></a>
-            <a href="./contact.php"><button class="white-button">Contact us</button></a>
             <a href="./profile.php"><button class="white-button">Profile</button></a>
+            
+           
+            <a href="./admin.php"><button class="white-button">Admin Dashboard</button></a>
+            
             <a href="./logout.php"><button class="green-button">Logout</button></a>
         <?php } ?>
-    </div>
-        
     </div>
 </nav>
 

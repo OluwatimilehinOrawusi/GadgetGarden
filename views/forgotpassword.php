948a5ad8f75cbd2c428710f5321d0f2f9b54b35c
Luca Corcione <230210859@aston.ac.uk>
2025-03-22 01:03:50 +0000

    fixed navbar on forgotpassword.php

--- views/forgotpassword.php
+++ views/forgotpassword.php
@@ -32,24 +32,27 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </head>
 <body>
 <nav>
-            <div class="nav-left">
-                <a href="../index.php"><p id="logo-text">GADGET GARDEN</p></a>
-            </div>
-            <div class="nav-right">
-                <a href="../views/aboutpage.php"><button class="white-button">About Us</button></a>
-                <?php if (!isset($_SESSION['user_id'])){?>
-                <?php echo '<a href="./login.php"><button class="green-button">Login</button></a>' ?>
-                 <?php echo '<a href="./signup.php"><button class="white-button">Sign Up</button></a> '?>
-                <?php }?>
-                <?php if (isset($_SESSION['user_id'])){?>
-                <?php echo '<a href="./basket.php"><button class="white-button">Basket</button></a>' ?>
-                <?php echo '<a href="./contact.php"><button class="white-button">Contact us</button></a>' ?>
-                <?php echo '<a href = "./profile.php"><button class ="white-button">Profile</button></a>' ?>
-                <?php echo '<a href="./logout.php"><button class="green-button">Logout</button></a>' ?>
-
-                <?php }?>
-
-            </div>
+    <div class="nav-left">
+        <a href="../index.php"><p id="logo-text">GADGET GARDEN</p></a>
+    </div>
+    <div class="nav-right">
+        <a href="../views/contact.php"><button class="green-button">Contact Us</button></a>
+        <a href="../views/aboutpage.php"><button class="white-button">About Us</button></a>
+        <?php if (!isset($_SESSION['user_id'])) { ?>
+            <a href="./login.php"><button class="green-button">Login</button></a>
+            <a href="./signup.php"><button class="white-button">Sign Up</button></a>
+        <?php } ?>
+        <a href="../views/products.php"><button class="green-button">Products</button></a>
+        <?php if (isset($_SESSION['user_id'])) { ?>
+            <a href="./basket.php"><button class="white-button">Basket</button></a>
+            <a href="./profile.php"><button class="white-button">Profile</button></a>
+            
+           
+            <a href="./admin.php"><button class="white-button">Admin Dashboard</button></a>
+            
+            <a href="./logout.php"><button class="green-button">Logout</button></a>
+        <?php } ?>
+    </div>
 </nav>
 <div id = "login-page">
     <div id = "leftside-container">

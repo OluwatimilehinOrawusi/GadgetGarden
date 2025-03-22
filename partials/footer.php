8172060af8e2cc72dc3d68cde84884ce5c437969
Kojo6771 <230165858@aston.ac.uk>
2025-03-22 20:04:22 +0000

    correctly linked audio on footer

--- partials/footer.php
+++ partials/footer.php
@@ -10,7 +10,7 @@
                 <a class="footer-link" href="./views/products.php?category=phones">Phones</a>
                 <a class="footer-link" href="./views/products.php?category=tablets">Tablets</a>
                 <a  class="footer-link"href="./views/products.php?category=laptops">Laptops</a>
-                <a  class="footer-link"href="./products.php?category=audio">Audio</a>
+                <a  class="footer-link"href="./views/products.php?category=audio">Audio</a>
                 <a class="footer-link" href="./products.php?category=accessories">Accessories</a>
             </div>
             <div class="footer-column">

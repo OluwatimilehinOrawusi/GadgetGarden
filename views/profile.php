e78b7aedfab8a613737590d5abc694c0b85717f9
Luca Corcione <230210859@aston.ac.uk>
2025-03-22 02:16:42 +0000

    added final navbar to profile.php

--- views/profile.php
+++ views/profile.php
@@ -55,24 +55,28 @@ $orders = $orderQuery->fetchAll(PDO::FETCH_ASSOC);
 <body>
 <nav>
     <div class="nav-left">
-        <a href="../index.php"><p id="logo-text">GADGET GARDEN</p></a>
+        <p id="logo-text">GADGET GARDEN</p>
     </div>
     <div class="nav-right">
-        <a href="../views/contact.php"><button class="green-button">Contact Us</button></a>
-        <a href="../views/aboutpage.php"><button class="white-button">About Us</button></a>
+        <a href="./aboutpage.php"><button class="white-button">About Us</button></a>
+
         <?php if (!isset($_SESSION['user_id'])) { ?>
             <a href="./login.php"><button class="green-button">Login</button></a>
             <a href="./signup.php"><button class="white-button">Sign Up</button></a>
         <?php } ?>
-        <a href="../views/products.php"><button class="green-button">Products</button></a>
+
         <?php if (isset($_SESSION['user_id'])) { ?>
-            <a href="./basket.php"><button class="white-button">Basket</button></a>
+            <a href="./basket.php"><button class="green-button">Basket</button></a>
+            <a href="./contact.php"><button class="green-button">Contact Us</button></a>
             <a href="./profile.php"><button class="white-button">Profile</button></a>
-            
-           
-            <a href="./admin.php"><button class="white-button">Admin Dashboard</button></a>
-            
+            <a href="./products.php"><button class="green-button">Products</button></a>
+
+            <?php if ($user && ($user['role'] === 'admin' || $user['role'] === 'manager')){ ?>
+                <a href="./dashboard.php"><button class="white-button">Admin Dashboard</button></a>
+            <?php } ?>
+
             <a href="./logout.php"><button class="green-button">Logout</button></a>
+        
         <?php } ?>
     </div>
 </nav>

5975bc9f1aab456e9d0540c678a56d27220f6000
Luca Corcione <230210859@aston.ac.uk>
2025-03-23 22:01:23 +0000

    added debugging code to contact.php

--- views/contact.php
+++ views/contact.php
@@ -1,4 +1,9 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start(); // Start session
 
 $pdo = require_once "../database/database.php"; // Connect to the database
